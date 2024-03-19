e35d2df96d9d28a44206c5861b9cc53a7f8162c8
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2024-03-19 15:06:59 +0700

    response with received payload

--- resolvers/hook.ts
+++ resolvers/hook.ts
@@ -2,5 +2,6 @@ export const handleIncomingHook = async (payload: object) => {
 	console.log(payload, '<-- payload from incoming hook');
 	return {
 		message: 'success, incoming hook called',
+		received: payload,
 	};
 };
