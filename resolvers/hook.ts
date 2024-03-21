ea7e8c137a9e26d72012729aab8b158ff7a4f832
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2024-03-21 11:26:27 +0700

    relocate hook into/as reward

--- resolvers/hook.ts
+++
@@ -1,7 +0,0 @@
-export const handleIncomingHook = async (payload: object) => {
-	console.log(payload, '<-- payload from incoming hook');
-	return {
-		message: 'success, incoming hook called',
-		received: payload,
-	};
-};
---
+++ resolvers/reward.ts
@@ -0,0 +1,34 @@
+import BN from 'bn.js';
+
+export interface QuickomResource {
+	time?: number;
+	banwhich?: number;
+}
+
+export interface SendRewardPayload {
+	amount: string /* note: receive bn.js string-value */;
+	walletAddress: string;
+	resource?: QuickomResource;
+}
+
+export const handleSendReward = async (payload: SendRewardPayload) => {
+	let parsedAmount: BN;
+	const { amount, walletAddress } = payload;
+
+	if (!walletAddress) throw new Error('missing walletAddress param!');
+	if (!amount) throw new Error('missing amount param!');
+
+	try {
+		parsedAmount = new BN(amount);
+		console.log('parsedAmount', parsedAmount);
+	} catch {
+		throw new Error('invalid amount, must be bn.js value in string!');
+	}
+
+	if (walletAddress === null) throw new Error('invalid wallet address');
+	return {
+		message: 'success, incoming hook called',
+		received: payload,
+		parsedAmount,
+	};
+};
